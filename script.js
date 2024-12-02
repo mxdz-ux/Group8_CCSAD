f5dcba90b8d78cf04df86ece9a4fb735c0e623b7
JRam1219 <j.ramil1219@gmail.com>
2024-12-03 01:02:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+JAVASCRIPT NAMAN TO
+
+const swiper = new Swiper (".slider-container" , {
+    effect: "slide",
+    speed: 1300,
+    // autoplay: {delay: 4000}
+    navigation: {
+        prevEl: "#slide-prev" ,
+        nextEl: "#slide-next"
+    } ,
+    });
